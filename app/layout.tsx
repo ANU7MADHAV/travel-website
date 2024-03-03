6d8815e1ffc8a10bf9e8c308abaa03d6923f9409
ANU MADHAV <150779776+ANU7MADHAV@users.noreply.github.com>
2024-03-03 16:17:20 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import Footer from "@/components/footer/Footer";
+import Footer from "@/components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
